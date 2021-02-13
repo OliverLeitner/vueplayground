14b0b14259f2aa23cfbc25f069bcafb8521110f8
Oliver Leitner <never@neverslair-blog.net>
2021-02-13 18:49:16 +0100

    few debug preparations

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,15 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App) //.use(store).use(router).mount('#app')
+
+app.use(store)
+app.use(router)
+
+/*if (process.env.NODE_ENV === 'development') {
+    if ('__VUE_DEVTOOLS_GLOBAL_HOOK__' in window) {
+        window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = app
+    }
+}*/
+
+app.mount('#app')
