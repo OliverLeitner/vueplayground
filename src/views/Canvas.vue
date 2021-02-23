9526b141e5cf71323519cb4061c91de9fe5681a0
Oliver Leitner <never@neverslair-blog.net>
2021-02-23 05:10:57 +0100

    canvas calling shorter

--- src/views/Canvas.vue
+++ src/views/Canvas.vue
@@ -3,9 +3,9 @@
     <h1>This is an about page</h1>
   </div>
   <h4>camera position</h4>
-  x: <input type="number" v-model.number="camX" />&nbsp;
-  y: <input type="number" v-model.number="camY" />&nbsp;
-  z: <input type="number" v-model.number="camZ" />
+  x: <input type="number" v-model.number="camPosition.x" />&nbsp;
+  y: <input type="number" v-model.number="camPosition.y" />&nbsp;
+  z: <input type="number" v-model.number="camPosition.z" />
   <br />
   <br />
   <RenderCanvas :camPosition="camPosition" />
@@ -20,17 +20,6 @@ import RenderCanvas from "@/components/RenderCanvas.vue"; // @ is an alias to /s
   components: {
     RenderCanvas,
   },
-  watch: {
-    camX: function (camX) {
-      this.submitData(camX, this.camY, this.camZ)
-    },
-    camY: function (camY) {
-      this.submitData(this.camX, camY, this.camZ)
-    },
-    camZ: function (camZ) {
-      this.submitData(this.camX, this.camY, camZ)
-    }
-  },
   data() {
     // initial values
     return {
@@ -39,16 +28,7 @@ import RenderCanvas from "@/components/RenderCanvas.vue"; // @ is an alias to /s
       camY: 5,
       camZ: 5
     }
-  },
-  methods: {
-    submitData(x: number, y: number, z: number) {
-      if (x && y && z) {
-        this.camPosition.x = x
-        this.camPosition.y = y
-        this.camPosition.z = z
-      }
-    }
-  },
+  }
 })
 export default class AboutView extends Vue {}
 </script>
